d40237cab5559c786d1bb7231a1c0a773e9c3f54
Sylvanusboni <sylvanus.boni@epitech.eu>
2025-02-21 10:35:02 +0100

    .

--- script/music.js
+++ script/music.js
@@ -13,3 +13,28 @@ playPauseBtn.addEventListener("click", () => {
     icon.classList.add("fa-play");
   }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const music = document.getElementById("bg-music");
+    const playPauseBtn = document.getElementById("play-pause");
+    const icon = playPauseBtn.querySelector("i");
+  
+    if (!music) {
+      console.error("🎵 L'élément audio n'a pas été trouvé !");
+      return;
+    }
+  
+    playPauseBtn.addEventListener("click", () => {
+      if (music.paused) {
+        music.play().then(() => {
+          icon.classList.remove("fa-play");
+          icon.classList.add("fa-pause");
+        }).catch(error => console.error("🔴 Erreur de lecture :", error));
+      } else {
+        music.pause();
+        icon.classList.remove("fa-pause");
+        icon.classList.add("fa-play");
+      }
+    });
+  });
+  
