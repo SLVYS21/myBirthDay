20ddd084f974ae6385c331daaa7b938a5d4e90da
Sylvanusboni <sylvanus.boni@epitech.eu>
2025-02-23 21:31:07 +0100

    .

--- script/music.js
+++ script/music.js
@@ -2,11 +2,27 @@ const music = document.getElementById("bg-music");
 const playPauseBtn = document.getElementById("play-pause");
 const icon = playPauseBtn.querySelector("i");
 
+// playPauseBtn.addEventListener("click", () => {
+//   if (music.paused) {
+//     music.play().catch(error => console.log("Erreur de lecture:", error));
+//     icon.classList.remove("fa-play");
+//     icon.classList.add("fa-pause");
+//   } else {
+//     music.pause();
+//     icon.classList.remove("fa-pause");
+//     icon.classList.add("fa-play");
+//   }
+// });
+
 playPauseBtn.addEventListener("click", () => {
+  checkAudioPermission();
   if (music.paused) {
-    music.play().catch(error => console.log("Erreur de lecture:", error));
-    icon.classList.remove("fa-play");
-    icon.classList.add("fa-pause");
+    music.play()
+      .then(() => {
+        icon.classList.remove("fa-play");
+        icon.classList.add("fa-pause");
+      })
+      .catch(error => console.error("🔴 Erreur de lecture :", error));
   } else {
     music.pause();
     icon.classList.remove("fa-pause");
@@ -14,27 +30,27 @@ playPauseBtn.addEventListener("click", () => {
   }
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-    const music = document.getElementById("bg-music");
-    const playPauseBtn = document.getElementById("play-pause");
-    const icon = playPauseBtn.querySelector("i");
+// document.addEventListener("DOMContentLoaded", () => {
+//     const music = document.getElementById("bg-music");
+//     const playPauseBtn = document.getElementById("play-pause");
+//     const icon = playPauseBtn.querySelector("i");
   
-    if (!music) {
-      console.error("🎵 L'élément audio n'a pas été trouvé !");
-      return;
-    }
+//     if (!music) {
+//       console.error("🎵 L'élément audio n'a pas été trouvé !");
+//       return;
+//     }
   
-    playPauseBtn.addEventListener("click", () => {
-      if (music.paused) {
-        music.play().then(() => {
-          icon.classList.remove("fa-play");
-          icon.classList.add("fa-pause");
-        }).catch(error => console.error("🔴 Erreur de lecture :", error));
-      } else {
-        music.pause();
-        icon.classList.remove("fa-pause");
-        icon.classList.add("fa-play");
-      }
-    });
-  });
+//     playPauseBtn.addEventListener("click", () => {
+//       if (music.paused) {
+//         music.play().then(() => {
+//           icon.classList.remove("fa-play");
+//           icon.classList.add("fa-pause");
+//         }).catch(error => console.error("🔴 Erreur de lecture :", error));
+//       } else {
+//         music.pause();
+//         icon.classList.remove("fa-pause");
+//         icon.classList.add("fa-play");
+//       }
+//     });
+//   });
   
